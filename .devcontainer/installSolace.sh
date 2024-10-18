742b3fc200d1bd000072e12f6915a1eb905798e3
Tamimi <tamimi@solace.com>
2024-10-18 09:17:51 -0700

    update

--- .devcontainer/installSolace.sh
+++ .devcontainer/installSolace.sh
@@ -3,4 +3,9 @@ while (! docker info > /dev/null 2>&1); do
   echo "Waiting for Docker to start..."
   sleep 2
 done
+
 docker run -d -p 8080:8080 -p 55555:55555 -p 8008:8008 -p 1883:1883 -p 8000:8000 -p 5672:5672 -p 9000:9000 -p 2223:2222 --shm-size=2g --env username_admin_globalaccesslevel=admin --env username_admin_password=admin --name=solace solace/solace-pubsub-standard
+
+echo "deb [arch=amd64 trusted=yes] https://raw.githubusercontent.com/SolaceLabs/apt-stm/master stm main" | sudo tee  /etc/apt/sources.list.d/solace-stm-test.list
+sudo apt-get update
+sudo apt-get install stm
