4881fbf9793f17733874ae5e0933a26b4ab1d49a
Tamimi <tamimi@solace.com>
2024-10-18 08:58:00 -0700

    update check for docker

--- .devcontainer/installSolace.sh
+++ .devcontainer/installSolace.sh
@@ -1,1 +1,6 @@
-docker run -d -p 8080:8080 -p 55555:55555 -p 8008:8008 -p 1883:1883 -p 8000:8000 -p 5672:5672 -p 9000:9000 -p 2222:2222 --shm-size=2g --env username_admin_globalaccesslevel=admin --env username_admin_password=admin --name=solace solace/solace-pubsub-standard
+#!/bin/bash
+while (! docker info > /dev/null 2>&1); do
+  echo "Waiting for Docker to start..."
+  sleep 2
+done
+docker run -d -p 8080:8080 -p 55555:55555 -p 8008:8008 -p 1883:1883 -p 8000:8000 -p 5672:5672 -p 9000:9000 -p 2223:2222 --shm-size=2g --env username_admin_globalaccesslevel=admin --env username_admin_password=admin --name=solace solace/solace-pubsub-standard
