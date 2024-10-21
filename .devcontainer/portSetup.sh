4614b8764c189ff8dd50c97471622842ce7f676e
Tamimi <tamimi@solace.com>
2024-10-21 10:21:09 -0700

    update

--- .devcontainer/portSetup.sh
+++ .devcontainer/portSetup.sh
@@ -1,2 +1,4 @@
 echo "Exposing port 8080"
 gh codespace ports visibility 8080:public -c $CODESPACE_NAME
+gh codespace ports visibility 1443:public -c $CODESPACE_NAME
+gh codespace ports visibility 8008:public -c $CODESPACE_NAME
